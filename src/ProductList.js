38a66907cb90a2b5c99a1306caa05cb8e8b4c100
burak.caglayan@outlook.com <burak.caglayan@outlook.com>
2020-05-15 18:19:07 +0300

    Add useState

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { useState } from 'react'
 
-const ProductList = ({parentMessage}) => {
+const ProductList = ({ parentMessage }) => {
+    const [count, setCount] = useState(0);
 
     return (
         <div>
             ProductList <b>{parentMessage}</b>
+            <br />
+            <label>{count}</label>
+            <button onClick={() => setCount(count + 1)}> + </button>
         </div>
     );
 }
